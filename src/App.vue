d5be426311639f2744ddc481120e0065b1e001a5
DanielCarroll <danielcarroll95@gmail.com>
2016-12-21 19:25:11 -0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <navbar></navbar>
+    <home></home>
+  </div>
+</template>
+
+<script>
+import Hello from './components/Hello'
+import Home from './components/Home'
+import Navbar from './components/reusable/Navbar'
+
+export default {
+  name: 'app',
+  components: {
+    Hello,
+    Home,
+    Navbar
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class='container'>
+        <div class='row'>
+            <div class='col-md-3'>
+                <img src='../assets/dirtyboi.jpg' class="img-fluid rounded-circle">
+                <h3>{{title}}</h3>
+                <p>Insert some dramatic biography stuff here</p>
+            </div>
+            
+            <div class='col-md-9'>
+                <div class='card'>
+                    Example card
+                </div>
+            </div>
+        </div>    
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'home',
+    data () {
+        return{
+          title: 'Clean Dan'
+        }
+    }
+}
+</script>
+
